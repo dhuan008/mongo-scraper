655d1586ddcaa6d4deaee4ed4b2d07f08c5db84e
David Huang <daveshuang@gmail.com>
2019-08-23 18:01:49 -0700

    added empty and panel functions

---
+++ public/assets/javascript/index.js
@@ -0,0 +1,70 @@
+$(document).ready(() => {
+    const articleContainer = $('.article-container');
+    $(document).on('click', '.btn.save', handleArticleSave);
+    $(document).on('click', '.scrape-new', handleArticleScrape);
+
+    initPage();
+
+    const initPage = () => {
+        articleContainer.empty();
+        $.get('/api/headlines?saved=false')
+            .then(data => {
+                if (data && data.length) {
+                    renderArticles(data);
+                }
+                else {
+                    renderEmpty();
+                }
+            });
+    }
+
+    const renderArticles = articles => {
+        const articlePanels = [];
+
+        for (let i = 0; i < articles.length; i++) {
+            articlePanels.push(createPanel(articles[i]));
+        }
+        articleContainer.append(articlePanels);
+    }
+
+    const createPanel = article => {
+        let panel =
+            $(["<div class='panel panel-default'>",
+                "<div class='panel-heading'>",
+                "<h3>",
+                article.headline,
+                "<a class='btn btn-success save'>",
+                "Save Article",
+                "</a>",
+                "</h3>",
+                "</div>",
+                "<div class='panel-body'>",
+                article.summary,
+                "</div>",
+                "</div>"
+            ].join(""));
+
+        panel.data('_id', article._id);
+        return panel;
+    }
+
+    const renderEmpty = () => {
+        let emptyAlert =
+            $(["<div class='alert alert-warning text-center'>",
+                "<h4>No new Articles</h4>",
+                "</div>",
+                "<div class='panel panel-default'>",
+                "<div class='panel-heading text-center'>",
+                "<h3>What would you like to do?</h3>",
+                "</div>",
+                "<div class='panel-body text-center'>",
+                "<h4><a class'scrape-new'>Try Scraping New Articles</a></h4>",
+                "<h4><a href='/saved'>Go to Saved Articles</a></h4>",
+                "</div>",
+                "</div>"
+            ].join(""));
+
+        articleContainer.append(emptyAlert);
+    }
+
+});
